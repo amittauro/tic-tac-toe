a6185b5efb63f2555f744498ea705c9191b98953
Amit Tauro <tauroamit@gmail.com>
2021-01-13 15:30:50 +0000

    refactoring won method

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -20,20 +20,26 @@ class TicTacToe
     return 'Player1 has won the game' if player1_won?
   end
 
-  def player1_won?
-    3.times do |column|
-      return true if column_won?(column)
-    end
-    3.times do |row|
-      return true if row_won?(row)
-    end
-    diagonal_won?
-  end
-
   private
 
   def create_board
     Array.new(3, nil).map{ |row| Array.new(3, nil) }
+  end
+
+  def player1_won?
+    any_column_won? || any_row_won? || any_diagonal_won?
+  end
+
+  def any_column_won?
+    3.times do |column|
+      return true if column_won?(column)
+    end
+  end
+
+  def any_row_won?
+    3.times do |row|
+      return true if row_won?(row)
+    end
   end
 
   def column_won?(column)
@@ -44,7 +50,7 @@ class TicTacToe
     board[row][0] == 'x' and board[row][1] == 'x' and board[row][2] == 'x'
   end
 
-  def diagonal_won?
+  def any_diagonal_won?
     (board[0][0] == 'x' and board[1][1] == 'x' and board[2][2]) == 'x' || (
       board[2][0] == 'x' and board[1][1] == 'x' and board[0][2] == 'x')
   end
