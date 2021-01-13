ceea4af6610bea0a1faa5ca667d1342700072d66
Amit Tauro <tauroamit@gmail.com>
2021-01-13 14:58:31 +0000

    test for player1won for one column

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,17 +1,23 @@
 class TicTacToe
 
+  attr_reader :board
+
   def initialize
     @board = create_board
   end
 
   def player1move(row, column)
-    @board[row][column] = 'x'
-    @board
+    board[row][column] = 'x'
+    board
   end
 
   def player2move(row, column)
-    @board[row][column] = 'o'
-    @board
+    board[row][column] = 'o'
+    board
+  end
+
+  def player1won?
+    board[0][0] == 'x' and board[1][0] == 'x' and board[2][0] == 'x'
   end
 
   private
--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -25,4 +25,15 @@ describe TicTacToe do
       end
     end
   end
+
+  describe '#player1won?' do
+    context 'when player1 has won the game for a column' do
+      it 'returns true' do
+        tic_tac_toe.player1move(0, 0)
+        tic_tac_toe.player1move(1, 0)
+        tic_tac_toe.player1move(2, 0)
+        expect(tic_tac_toe.player1won?).to eq(true)
+      end
+    end
+  end
 end
