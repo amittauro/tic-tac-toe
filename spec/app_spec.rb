fed7960b4f4f1632b485ffd8d05f51b8ec8882ae
Amit Tauro <tauroamit@gmail.com>
2021-01-18 17:37:37 +0000

    updating app

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -12,6 +12,16 @@ describe App do
         expect(object).to receive(:run)
         app.run
       end
+
+      it 'runs tic tac toe' do
+        player = double('player', new: nil)
+        tic_tac_toe = double('tic_tac_toe')
+        object = double('object')
+        app = App.new(player, tic_tac_toe)
+        allow(tic_tac_toe).to receive(:new).and_return(object)
+        allow(object).to receive(:run).and_return('tic tac toe running')
+        expect(app.run).to eq('tic tac toe running')
+      end
     end
   end
 end
